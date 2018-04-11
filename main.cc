478c55570c490e18dd914848e425718192311409
Víctor López Ferrando <victorlopez90@gmail.com>
2018-04-11 13:08:56 +0200

    Improved comments and translated all (comments and variables) to English

--- main.cc
+++ main.cc
@@ -8,90 +8,90 @@ const int WIDTH = 68,
           HEIGHT = 68,
           A = 3,
           B = 5,
-          N_QUADRATS = (WIDTH * HEIGHT) / (A*A + B*B);
+          N_SQUARES = (WIDTH * HEIGHT) / (A*A + B*B);
 
-class Quadrat {
-  public:
+struct Quadrat {
     uint8_t nA = 0, nB = 0;
-    vector<uint8_t> altura = {0}, repeats = {WIDTH};
-    bitset<2*N_QUADRATS> tria;
+    vector<uint8_t> height = {0}, length = {WIDTH}; // Intervals
+    bitset<2*N_SQUARES> insertions;
     size_t hash_value;
 
     bool operator ==(const Quadrat & q) const {
       return hash_value == q.hash_value;
     }
 
-    bool afegir(uint8_t mida) {
-      /* Afegir element al vector on toque */
-      size_t i = distance(altura.begin(), min_element(altura.begin(), altura.end()));
+    bool add_square(uint8_t size) {
+      /* Find min height interval */
+      size_t i = distance(height.begin(), min_element(height.begin(), height.end()));
 
-      // Massa petit
-      if (repeats[i] < mida)
+      /* Interval is too small */
+      if (length[i] < size)
         return false;
 
-      // Més gran
-      if (repeats[i] > mida) {
-        // Reduir mida del 0
-        repeats[i] -= mida;
+      /* Interval is bigger: the square will be added to the left */
+      if (length[i] > size) {
+        /* Reduce current interval length */
+        length[i] -= size;
 
-        // Early kill
-        if (repeats[i] == 1 or repeats[i] == 2 or repeats[i] == 4 or repeats[i] == 7)
+        /* Early kill if the new minimum interval length can't be reached */
+        if (length[i] == 1 or length[i] == 2 or length[i] == 4 or length[i] == 7)
           return false;
 
-        if (i > 0 and altura[i-1] == altura[i] + mida) {
-          // Juntar amb l'anterior
-          repeats[i-1] += mida;
+        if (i > 0 and height[i-1] == height[i] + size) {
+          /* Join the previous interval if heights match */
+          length[i-1] += size;
         }
         else {
-          // Inserir si a l'inici o l'anterior és diferent
-          altura.insert(altura.begin() + i, altura[i] + mida);
-          repeats.insert(repeats.begin() + i, mida);
+          /* Create a new interval otherwise */
+          height.insert(height.begin() + i, height[i] + size);
+          length.insert(length.begin() + i, size);
         }
       }
-      // Igual mida
-      else if (repeats[i] == mida) {
-        // Juntar els 3
-        if (i > 0 and altura[i-1] == altura[i] + mida and i+1 < altura.size() and altura[i+1] == altura[i] + mida) {
-          repeats[i-1] += mida + repeats[i+1];
-          altura.erase(altura.begin()+i, altura.begin()+i+2);
-          repeats.erase(repeats.begin()+i, repeats.begin()+i+2);
+      /* Interval is the same size as the square to be added */
+      else if (length[i] == size) {
+        if (i > 0 and height[i-1] == height[i] + size and i+1 < height.size() and height[i+1] == height[i] + size) {
+          /* Combine all three intervals if their height match */
+          length[i-1] += size + length[i+1];
+          height.erase(height.begin()+i, height.begin()+i+2);
+          length.erase(length.begin()+i, length.begin()+i+2);
         }
-        // Juntar a l'esquerra
-        else if (i > 0 and altura[i-1] == altura[i] + mida) {
-          repeats[i-1] += mida;
-          altura.erase(altura.begin() + i);
-          repeats.erase(repeats.begin() + i);
+        else if (i > 0 and height[i-1] == height[i] + size) {
+          /* Combine with the left interval */
+          length[i-1] += size;
+          height.erase(height.begin() + i);
+          length.erase(length.begin() + i);
         }
-        // Juntar a la dreta
-        else if (i+1 < altura.size() and altura[i+1] == altura[i] + mida) {
-          repeats[i+1] += mida;
-          altura.erase(altura.begin() + i);
-          repeats.erase(repeats.begin() + i);
+        else if (i+1 < height.size() and height[i+1] == height[i] + size) {
+          /* Combine with the right interval */
+          length[i+1] += size;
+          height.erase(height.begin() + i);
+          length.erase(length.begin() + i);
         }
-        // Canviar mida
-        else
-          altura[i] += mida;
+        else {
+          /* Increase the interval's height otherwise */
+          height[i] += size;
+        }
       }
 
-      /* Increment nA or nB and add to tria */
-      tria[mida == A ? nA++ : nB++] = (mida == A);
-      if (nA > N_QUADRATS or nB > N_QUADRATS)
+      /* Increment nA or nB and add to insertions bitset */
+      insertions[size == A ? nA++ : nB++] = (size == A);
+      if (nA > N_SQUARES or nB > N_SQUARES)
         return false;
 
-      /* Flip skyline if necessary */
-      for (size_t i = 0; i < altura.size()/2; ++i)
-        if (altura[i] != altura[altura.size()-i-1]) {
-          if (altura[i] < altura[altura.size()-i-1]) {
-            altura = vector<uint8_t>(altura.rbegin(), altura.rend());
-            repeats = vector<uint8_t>(repeats.rbegin(), repeats.rend());
+      /* Flip intervals if necessary */
+      for (size_t i = 0; i < height.size()/2; ++i)
+        if (height[i] != height[height.size()-i-1]) {
+          if (height[i] > height[height.size()-i-1]) {
+            height = vector<uint8_t>(height.rbegin(), height.rend());
+            length = vector<uint8_t>(length.rbegin(), length.rend());
           }
           break;
         }
 
       /* Compute hash and save in object */
       string s;
-      s.insert(end(s), begin(altura), end(altura));
-      s.insert(end(s), begin(repeats), end(repeats));
+      s.insert(end(s), begin(height), end(height));
+      s.insert(end(s), begin(length), end(length));
       s.push_back(nA);
       hash<string> hash_fn;
       hash_value = hash_fn(s);
@@ -101,11 +101,11 @@ class Quadrat {
 };
 
 ostream& operator<<(ostream& os, const Quadrat& q) {
-  for (size_t i = 0; i < q.altura.size(); ++i)
-    for (int j = 0; j < q.repeats[i]; ++j)
-      os << int(q.altura[i]) << " ";
+  for (size_t i = 0; i < q.height.size(); ++i)
+    for (int j = 0; j < q.length[i]; ++j)
+      os << int(q.height[i]) << " ";
   for (int i = 0; i < q.nA + q.nB; ++i)
-    os << (q.tria[i] ? B : A);
+    os << (q.insertions[i] ? B : A);
   return os;
 }
 
@@ -115,8 +115,7 @@ struct Hash {
   }
 };
 
-
-void write_square(bitset<2*N_QUADRATS> ordre) {
+void write_square(bitset<2*N_SQUARES> ordre) {
   cout << "TODO" << endl;
 }
 
@@ -127,24 +126,24 @@ int main() {
   unordered_set<Quadrat, Hash> set1, set2;
   set1.insert(Quadrat());
 
-  for (int n = 0; n < 2*N_QUADRATS; ++n) {
+  for (int n = 0; n < 2*N_SQUARES; ++n) {
     cerr << "Step " << n << ": " << set1.size() << " profiles." << endl;
 
     for (auto it = set1.cbegin(); it != set1.cend(); set1.erase(it++)) {
       // cerr << *it << endl;
       Quadrat q1(*it), q2(*it);
-      if (q1.afegir(A))
+      if (q1.add_square(A))
         set2.insert(q1);
-      if (q2.afegir(B))
+      if (q2.add_square(B))
         set2.insert(q2);
     }
 
     swap(set1, set2);
-    if (n == 40) break;
+    // if (n == 40) break;
   }
 
   if (not set1.empty()) {
     cout << *set1.begin() << endl;
-    write_square(set1.begin()->tria);
+    write_square(set1.begin()->insertions);
   }
 }
